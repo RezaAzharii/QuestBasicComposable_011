3713cb5b66f57a47ff143f937e1bfb61d35aca8d
RezaAzharii <rezaazhari867@gmail.com>
2024-10-18 21:03:28 +0700

    Menambahkan Foto profile

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -90,6 +92,9 @@ fun BasicCompose(modifier: Modifier = Modifier){
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
+        )
+        Image(painter = painterResource(id = R.drawable.mukagw),
+            contentDescription = null
         )
 
 
